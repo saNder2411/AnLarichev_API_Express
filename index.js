0a17e9f0810aee7a81e085fb4254fb3138a45612
Alexander Olshanskiy <sander2411ukr@gmail.com>
2022-01-10 22:47:48 +0200

    01_http

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const host = '127.0.0.1'
 
 const port = 8000
 
-const server = http.createServer((req, res) => {})
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World!')
+})
 
 server.listen(port, host, () => {
   console.log(`Server run on: ${host}:${port}`)
