d80205981c725c2b05fb2a6fbce77c2d822e1775
Alexander Olshanskiy <sander2411ukr@gmail.com>
2022-01-10 23:36:23 +0200

    res

--- index.js
+++ index.js
@@ -4,24 +4,22 @@ const PORT = 8000
 
 const app = express()
 
-app.all('/hello', (req, res, next) => {
-  console.log('All')
-  next()
+app.get('/hello', (req, res) => {
+  // res.status(201).json({ success: true })
+  // res.download('/test.pdf', 'tessst.pdf')
+  // res.redirect(301, 'https://example.com')
+  // Headers
+  // res.set('Content-Type', 'text/plain')
+  // res.append('Warning', 'code')
+  // res.type('application/json')
+  // res.location('url')
+  // res.links({ next: 'https://link', lastLink: 'https://link' })
+  // Cookies
+  // res.cookie('cookieName', 'shifr', { domain: '', path: '/', secure: true, expires: 600000 })
+  // res.clearCookie('cookieName', { path: '/' })
+
+  res.send('Hello World!')
 })
-
-const cb = (req, res, next) => {
-  console.log('cb')
-  next()
-}
-
-app.get('/hello', [
-  cb,
-  cb,
-  cb,
-  (req, res) => {
-    res.send('Hello World!')
-  },
-])
 
 app.listen(PORT, () => {
   console.log(`Server run on: http://localhost:${PORT}`)
