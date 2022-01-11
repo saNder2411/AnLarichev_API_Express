199be6f208056976ed6002160833b11b12941974
Alexander Olshanskiy <sander2411ukr@gmail.com>
2022-01-11 08:40:55 +0200

    meddleware

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const PORT = 8000
 
 const app = express()
 
+app.use((req, res, next) => {
+  console.log(Date.now())
+  next()
+})
+
 app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+  // res.send('Hello World!')
+  throw new Error('Error!')
 })
 
 app.use('/users', userRouter)
+
+app.use((err, req, res, next) => {
+  console.log(err.message)
+  res.status(500).send(err.message)
+})
 
 app.listen(PORT, () => {
   console.log(`Server run on: http://localhost:${PORT}`)
--- users/users.js
+++ users/users.js
@@ -2,6 +2,11 @@ import { Router } from 'express'
 
 export const userRouter = Router()
 
+userRouter.use((req, res, next) => {
+  console.log('users handler', Date.now())
+  next()
+})
+
 userRouter.post('/login', (req, res) => {
   res.send('login')
 })
