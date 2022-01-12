6eae06bbb8ee486dc1d0420cefd9f0061ba3d7b1
Alexander Olshanskiy <sander2411ukr@gmail.com>
2022-01-12 22:42:54 +0200

    arch_logger

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 import express, { Express, Router } from 'express'
 import { Server } from 'http'
-
-import { userRouter } from './users/users.js'
+// Instruments
+import { LoggerService } from './logger/logger.service'
+import { userRouter } from './users/users'
 
 export class App {
   app: Express
   server: Server
   port: number
+  logger: LoggerService
 
-  constructor() {
+  constructor(logger: LoggerService) {
     this.app = express()
 
     this.port = 8000
+
+    this.logger = logger
   }
 
   useRoutes() {
@@ -22,7 +26,7 @@ export class App {
     this.useRoutes()
 
     this.server = this.app.listen(this.port, () => {
-      console.log(`Server run on: http://localhost:${this.port}`)
+      this.logger.log(`Server run on: http://localhost:${this.port}`)
     })
   }
 }
---
+++ src/logger/logger.service.ts
@@ -0,0 +1,28 @@
+import { Logger } from 'tslog'
+
+// type MethodNames = 'info' | 'error' | 'warn'
+
+export class LoggerService {
+  private logger: Logger
+
+  constructor() {
+    this.logger = new Logger({
+      displayInstanceName: false,
+      displayLoggerName: false,
+      displayFilePath: 'hidden',
+      displayFunctionName: false,
+    })
+  }
+
+  log(...args: unknown[]) {
+    this.logger.info(...args)
+  }
+
+  error(...args: unknown[]) {
+    this.logger.error(...args)
+  }
+
+  warn(...args: unknown[]) {
+    this.logger.warn(...args)
+  }
+}
