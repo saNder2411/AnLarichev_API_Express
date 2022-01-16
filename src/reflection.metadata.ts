e205e3ab70c9369379a43c4516504b9b794e01d9
Alexander Olshanskiy <sander2411ukr@gmail.com>
2022-01-16 18:03:07 +0200

    reflect-metadata

---
+++ src/reflection.metadata.ts
@@ -0,0 +1,30 @@
+import 'reflect-metadata'
+
+const Injectable = (key: string) => (target: Function) => {
+  Reflect.defineMetadata(key, key, target)
+
+  const meta = Reflect.getMetadata(key, target)
+
+  console.log('meta', meta)
+}
+
+const Inject = (key: string) => (target: any, propName: string, index: number) => {
+  Reflect.defineMetadata(key, key, target)
+
+  const meta = Reflect.getMetadata(key, target)
+
+  console.log('meta', meta)
+}
+
+function Prop(target: Object, propName: string) {}
+
+@Injectable('C')
+export class C {
+  @Prop
+  prop: number
+}
+
+@Injectable('D')
+export class D {
+  constructor(@Inject('C') public c: C) {}
+}
